0dc07f2abc074a637ca708ab5580790fcffa6ec9
Lionel Schiepers <lionel_schiepers@hotmail.com>
2024-11-21 14:47:49 +0100

    install opentelemetry in a dedicated namespace

--- 2024024 - k8s - calico/otel.sh
+++ 2024024 - k8s - calico/otel.sh
@@ -2,9 +2,11 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-echo "Installing Open Telemetry"
+echo "Installing Open Telemetry Demo"
 whoami
 pwd
 
 helm repo add open-telemetry https://open-telemetry.github.io/opentelemetry-helm-charts
-helm install my-otel-demo open-telemetry/opentelemetry-demo
+helm repo update
+helm install otel-demo open-telemetry/opentelemetry-demo --create-namespace --namespace otel-demo --wait --timeout 10m
+echo "Finished Open Telemetry Demo"
