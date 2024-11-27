b68e635ce4d5b43cd710f484bf193795c38c7805
Lionel Schiepers <lionel_schiepers@hotmail.com>
2024-11-28 00:23:29 +0100

    create a test prpject in harbor

--- 2024024 - k8s - calico/harbor.sh
+++ 2024024 - k8s - calico/harbor.sh
@@ -67,16 +67,39 @@ spec:
     name: harbor-portal
 " | kubectl apply -f -
 
-# helm repo add jetstack https://charts.jetstack.io
-# helm repo update
+apt-get install docker docker-ce-cli jq
+# mkdir /etc/docker 2>/dev/null
+
+# if test -f /etc/default/docker; then
+#   echo "DOCKER_OPTS=\"--config-file=/etc/docker/daemon.json\"" > /etc/default/docker
+# fi
+
+# if test -f /etc/docker/daemon.json; then
+#   echo "{
+#   \"insecure-registries\":[\"$(ip address show eth0 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//' | tr -d '\n'):30002\"]
+# }" > /etc/docker/daemon.json
+# fi
 
 
-# helm install \
-#  cert-manager jetstack/cert-manager \
-#  --namespace cert-manager \
-#  --create-namespace \
-#  --version v1.16.2 \
-#  --set crds.enabled=true \
-#  --wait --timeout 2m
+# cat /etc/docker/daemon.json | jq '.insecure-registries += ["192.168.124.171:30002"]'
+
+# create a test project in harbor
+echo '{
+    "project_name": "test",
+    "public": true,
+    "metadata": {
+      "public": "true",
+      "enable_content_trust": "false",
+      "enable_content_trust_cosign": "false"
+    },
+    "storage_limit": 0
+  }' | curl -s -o /tmp/result -u "admin:password" -H 'accept: application/json' -H 'Content-Type: application/json' --data-binary @- -X POST http://$(ip address show eth0 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//' | tr -d '\n'):30002/api/v2.0/projects
+
+
+docker image pull debian
+docker image tag debian:latest 127.0.0.1:30002/test/debian:latest
+
+docker login 127.0.0.1:30002 -u=admin -p=password
+docker image push 127.0.0.1:30002/test/debian:latest
 
 echo "Finished Harbor"
