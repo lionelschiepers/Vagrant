24cd3d19c9205e2e93f446381fbe614cc61690ac
Lionel Schiepers <lionel_schiepers@hotmail.com>
2024-11-27 04:09:03 +0100

    install harbor as NodePort because it works out of the box for both portal & core.

--- 2024024 - k8s - calico/harbor.sh
+++ 2024024 - k8s - calico/harbor.sh
@@ -10,10 +10,18 @@ helm repo add harbor https://helm.goharbor.io
 helm repo update
 
 # https://vishynit.medium.com/setting-up-harbor-registry-on-kubernetes-using-helm-chart-5989d7c8df2a
-helm install harbor harbor/harbor --create-namespace --namespace harbor --wait --timeout 10m
+#helm install harbor harbor/harbor --create-namespace --namespace harbor --wait --timeout 10m \
+# --set harborAdminPassword=password \
+# --set externalURL=http://$(ip address show eth0 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//' | tr -d '\n') \
 
-export PROXY_IP=$(kubectl get svc --namespace kong kong-gateway-proxy -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-echo $PROXY_IP
+helm install harbor harbor/harbor --create-namespace --namespace harbor --wait --timeout 10m \
+ --set harborAdminPassword=password \
+ --set externalURL=http://$(ip address show eth0 | grep 'inet ' | sed -e 's/^.*inet //' -e 's/\/.*$//' | tr -d '\n'):30002 \
+ --set expose.type=nodePort \
+ --set expose.tls.enabled=false
+
+# export PROXY_IP=$(kubectl get svc --namespace kong kong-gateway-proxy -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+# echo $PROXY_IP
 
 echo "
 apiVersion: gateway.networking.k8s.io/v1
@@ -21,7 +29,7 @@ kind: HTTPRoute
 metadata:
   name: harbor
   annotations:
-    konghq.com/strip-path: 'true'
+    konghq.com/strip-path: 'false'
 spec:
   parentRefs:
   - name: kong
@@ -29,8 +37,13 @@ spec:
   - matches:
     - path:
         type: PathPrefix
-        value: /harbor
+        value: /c
     backendRefs:
+    - name: harbor-core
+      namespace: harbor
+      kind: Service
+      port: 80
+  - backendRefs:
     - name: harbor-portal
       namespace: harbor
       kind: Service
