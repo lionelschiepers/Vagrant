1ec58daa2266438fa4ecd986521d5d5f9eeeed38
Lionel Schiepers <lionel_schiepers@hotmail.com>
2025-01-07 21:47:31 +0100

    install step ca for certificate

---
+++ 2024024 - k8s - calico/autocert.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+echo "Installing autocert"
+whoami
+pwd
+
+wget https://dl.smallstep.com/cli/docs-ca-install/latest/step-cli_amd64.deb
+sudo dpkg -i step-cli_amd64.deb
+
+wget https://dl.smallstep.com/certificates/docs-ca-install/latest/step-ca_amd64.deb
+sudo dpkg -i step-ca_amd64.deb
+
+echo abc > capwd.txt
+step ca init --deployment-type=standalone --name=Licaya --dns=localhost --address=:8443 --provisioner=ls@toto.com --password-file=capwd.txt
+step certificate fingerprint .step/certs/root_ca.crt
+
+sudo step-ca .step/config/ca.json --password-file capwd.txt &
+
+step ca root root.crt -f
+step ca certificate localhost srv.crt srv.key --password-file capwd.txt -f 
+
+#or
+ step certificate create --csr foo.example.com foo.csr foo.key
+ step ca sign foo.csr foo.crt
+
+
+# helm repo add smallstep https://smallstep.github.io/helm-charts/
+# helm repo update
+# helm install step-certificates smallstep/step-certificates
+
+# https://github.com/smallstep/autocert 
+
+helm repo add smallstep https://smallstep.github.io/helm-charts/
+helm repo update
+helm install autocert smallstep/autocert -n autocert --create-namespace
+
+kubectl wait deployment -n autocert --all --for condition=Available=True --timeout=300s
+kubectl -n default logs job.batch/autocert
+
+echo "autocert installed"
